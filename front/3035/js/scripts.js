0f4e5f3fa38076752c947279d71f204d7e070327
CaoTranMinhTam0811 <tamcaotranminh@gmail.com>
2024-10-15 09:14:59 +0700

    update files

--- front/3035/js/scripts.js
+++ front/3035/js/scripts.js
@@ -1,19 +1,16 @@
 function openTab(evt, tabName) {
-    // Hide all tab content
+
     var tabContents = document.querySelectorAll('.tab-content');
     tabContents.forEach(function (tabContent) {
         tabContent.classList.remove('active');
     });
 
-    // Remove active class from all buttons
     var tabButtons = document.querySelectorAll('.tab button');
     tabButtons.forEach(function (tabButton) {
         tabButton.classList.remove('active');
     });
 
-    // Show the current tab
     document.getElementById(tabName).classList.add('active');
 
-    // Add active class to the button that opened the tab
     evt.currentTarget.classList.add('active');
 }
